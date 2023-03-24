ee6e58f7704a0285ecb367de52003afccf4fbe50
xiaomayihahale <126331536+xiaomayihahale@users.noreply.github.com>
2023-03-24 20:51:27 +0800

    Add files via upload

---
+++ wjyk_nhyk/vendor/cos-php-sdk-v5-v2.2.3/sample/putObject.php
@@ -0,0 +1,49 @@
+<?php
+
+require dirname(__FILE__) . '/../vendor/autoload.php';
+
+$secretId = "SECRETID"; //"云 API 密钥 SecretId";
+$secretKey = "SECRETKEY"; //"云 API 密钥 SecretKey";
+$region = "ap-beijing"; //设置一个默认的存储桶地域
+$cosClient = new Qcloud\Cos\Client(
+    array(
+        'region' => $region,
+        'schema' => 'https', //协议头部，默认为http
+        'credentials'=> array(
+            'secretId'  => $secretId ,
+            'secretKey' => $secretKey)));
+$local_path = "/data/exampleobject";
+//添加tagging
+/*$tagSet = http_build_query( array(
+    urlencode("key1") => urlencode("value1"),
+    urlencode("key2") => urlencode("value2")),
+    '',
+    '&'
+); */
+try {
+    $result = $cosClient->putObject(array(
+        'Bucket' => 'examplebucket-125000000', //格式：BucketName-APPID
+        'Key' => 'exampleobject',
+        'Body' => fopen($local_path, 'rb'),
+        /*
+        'CacheControl' => 'string',
+        'ContentDisposition' => 'string',
+        'ContentEncoding' => 'string',
+        'ContentLanguage' => 'string',
+        'ContentLength' => integer,
+        'ContentType' => 'string',
+        'Expires' => 'string',
+        'Metadata' => array(
+            'string' => 'string',
+        ),
+        'StorageClass' => 'string',
+        'Tagging' => $tagSet //最多10个标签
+        */
+    ));
+    // 请求成功
+    print_r($result);
+} catch (\Exception $e) {
+    // 请求失败
+    echo($e);
+}
+
